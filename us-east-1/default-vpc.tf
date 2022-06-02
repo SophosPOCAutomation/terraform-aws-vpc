00d13ea854b9229f226f284db838b1b17fed8d2d
RalphBrynard <ralph@thebrynards.com>
2022-06-02 12:46:27 -0400

    Initial Commit

---
+++ us-east-1/default-vpc.tf
@@ -0,0 +1,57 @@
+terraform {
+  cloud {
+    organization = "esg_se_xdr_demo_environment"
+    workspaces {
+      tags = ["us-east-1"]
+    }
+  }
+}
+
+provider "aws" {
+  region = ""
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "usw1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias  = "usw2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "use1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "use2"
+}
+
+locals {
+  import_date = formatdate("MM-DD-YYYY", timestamp())
+  default_tags = {
+    managed_by = "Terraform"
+    is_default = "true"
+    import_date = local.import_date
+  }
+}
+
+module "default_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+  # insert the 23 required variables here
+  providers = {
+    aws = aws.use1
+  }
+  create_vpc = false
+  manage_default_vpc = true
+  default_vpc_name = "default"
+  default_vpc_enable_dns_hostnames = true
+  tags = merge(
+    local.default_tags
+  )
+}
---
+++ us-east-2/default-vpc.tf
@@ -0,0 +1,46 @@
+provider "aws" {}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "usw1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias  = "usw2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "use1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "use2"
+}
+
+locals {
+  import_date = formatdate("MM-DD-YYYY", timestamp())
+  default_tags = {
+    managed_by = "Terraform"
+    is_default = "true"
+    import_date = local.import_date
+  }
+}
+
+module "default_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+  # insert the 23 required variables here
+   providers = {
+    aws = aws.use2
+  }
+  create_vpc = false
+  manage_default_vpc = true
+  default_vpc_name = "default"
+  default_vpc_enable_dns_hostnames = true
+  tags = merge(
+    local.default_tags
+  )
+}
